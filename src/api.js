420fcfe9aa96bd217f0d142f05e566493178d6ca
AlexGrigorenco <alpsnhandy@gmail.com>
2023-10-12 17:56:34 +0300

    get normal price from API

--- src/api.js
+++ src/api.js
@@ -2,9 +2,9 @@
 const API_KEY = '0c306692dcb0454412241eb93b1398b038e292c74c66c2ded01cfba0d11859eb'
 
 export function loadTickerPrice(tickersTitleList) {
-    return fetch(`https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${tickersTitleList}&api_key=${API_KEY}`)
+    return fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tickersTitleList}&tsyms=USD&api_key=${API_KEY}`)
     .then(response => response.json())
     .then(rawData => {
-        return Object.fromEntries(Object.entries(rawData).map(([key,value]) => [key, 1 / value]));
+        return Object.fromEntries(Object.entries(rawData).map(([key,value]) => [key, value.USD]));
     })
 }
